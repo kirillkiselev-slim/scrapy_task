d836f508a214067b2e8f4350c2ced17d9b069d55
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-09-21 20:54:25 +0300

    add constants

--- scrapy_task/scrapy_task/spiders/constants.py
+++ scrapy_task/scrapy_task/spiders/constants.py
@@ -6,4 +6,14 @@ USER_AGENT = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
 BASE_URL = 'http://free-proxy.cz/en'
 TOKEN = os.getenv('TOKEN')
 UPLOAD_URL = 'https://test-rg8.ddns.net/api/post_proxies'
+TOKEN_URL = 'https://test-rg8.ddns.net/api/get_token'
 BATCH_SIZE = 10
+PAYLOAD_POST_PROXIES = {
+    'user_id': TOKEN,
+}
+HEADERS_GET_TOKEN = headers = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Content-Type': 'application/json',
+}
